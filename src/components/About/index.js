06e54e0097e1bd04661f98cfa80345470c3b1be5
Sona <sonali23.1293@gmail.com>
2021-09-06 19:41:58 -0400

    about component

---
+++ src/components/About/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {Divider} from '@mui/material';
+import Hi from '../../HI.png';
+import Cat from '../../Cat.png';
+
+function About() {
+    const [skills] = React.useState([
+        { label: 'HTML', color:"purple-300"},
+        { label: 'CSS', color: "blue-300"},
+        { label: 'jQuery', color:"pink-300"},
+        { label: 'React', color: "indigo-300"},
+        { label: 'Node.js', color:"yellow-300" },
+        { label: "MongoDB", color: "green-300"},
+        { label: "Express", color: "gray-300"},
+        { label: "SQL", color: "yellow-500"}
+      ]);
+
+    return (
+    <section className="jumbotron text-center h-screen">
+        <div class="grid grid-cols-6 gap-4">
+            <div className="col-start-2 col-span-4">
+        <img src={Hi} alt="saying Hi" className="animate-bounce w-96 h-100 mx-auto pt-20 pb-10"/>
+        <img src={Cat} alt="Cartoon Cat" className="w-96 h-100 mx-auto pb-10"/>
+        <p className="inline mt-96 rounded-full font-light text-red-300 text-4xl tracking-widest">I am Sonali Pandey! A Full-Stack developer.</p>
+        </div>
+
+        <div className="col-start-1 col-end-7 pt-10">
+        <h3 className="text-5xl z-40 text-white text-red-300 h-16">My Skills</h3>
+        <ul className="pt-10">
+        {skills.map((skill) => (
+            <li
+            className={`inline text-3xl mx-4 my-4 shadow-lg text-white no-underline rounded-full py-3 px-6 bg-${skill.color}`}>
+                { skill.label }
+            </li>
+        ))}
+        </ul>
+        </div>
+        </div>
+    </section>
+
+    )
+}
+
+export default About;
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import {AppBar, Container, Toolbar, Typography, Divider, Stack} from '@mui/material';
 
 function Nav( props ) {
   const {
@@ -9,28 +8,19 @@ function Nav( props ) {
   } = props;
 
   return (
-    <AppBar position="static" elevation={0} sx={{ backgroundColor: "white"}}>
-        <Container fixed>
-            <Toolbar>
-                <Typography variant="h6" color="black" component="div" sx={{ flexGrow: 1 }}>Sonali Pandey</Typography>
-                <ul className="flex-row" sx={{ flexGrow: 1 }}>
-                    <Stack spacing={2} direction="row">
-                        {titles.map((title) => (
-                        <li 
-                            className={`navList ${currentTitle.name === title.name && 'navActive'}`}
-                            key={title.name}
-                        >
-                            <span color="black" onClick={() => { setCurrentTitle(title) }}>{ title.name }</span>
-                        </li>
-                        ))}
-                    </Stack>
-                </ul>
-            </Toolbar>
-      </Container>
-      <Divider />
-    </AppBar>
-
-
+    <nav className="sticky top-0 z-10 h-16 bg-white shadow-sm flex flex-wrap justify-between items-center">
+        <h2 className="ml-20 no-underline text-3xl text-red-300">Sonali Pandey</h2>
+        <ul className="mx-20 flex flex-wrap items-center">
+        {titles.map((title) => (
+            <li className={`cursor-pointer transform font-thin hover:scale-110 hover:text-red-600 ${currentTitle.name === title.name && 'font-normal text-red-300'}`} key={title.name}>
+                <span 
+                className="text-xl ml-3 mr-3 no-underline" 
+                onClick={() => { setCurrentTitle(title) }}>{ title.name }
+                </span>
+            </li>
+        ))}
+        </ul>
+    </nav>
   );
 }
 
